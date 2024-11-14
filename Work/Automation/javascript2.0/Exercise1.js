6e38fc87be6d94a6682cb64603d7558b77a78de5
Tal Baruchi <talbaruchy@gmail.com>
2024-11-14 14:56:04 +0200

    Exercise1

--- Work/Automation/javascript2.0/Exercise1.js
+++ Work/Automation/javascript2.0/Exercise1.js
@@ -1,1 +1,17 @@
-// Test
+//1.
+let fullName1 = "Tal Baruchi";
+var fullName2 = "Tal Baruchi";
+const fullName3 = "Tal Baruchi";
+console.log(fullName1, fullName2, fullName3);
+
+//2.
+let Age = 29;
+console.log(Age);
+Age = 30;
+console.log(Age);
+
+//3.
+const Bday = "17.07.1995";
+console.log(Bday);
+
+//4.
