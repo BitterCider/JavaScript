b6e2178ad7fc0ab8aeccbdcb5e3946562ade24dc
Tal Baruchi <talbaruchy@gmail.com>
2024-11-22 18:26:02 +0200

    לבדיקה תקיית JS

--- JavaScriptQA/JS/Loops.js
+++ JavaScriptQA/JS/Loops.js
@@ -1,37 +1,37 @@
-//1 
-function isCount(a){
-for (let i = 1; i <= 10; i++){
-    if (i === a && a % 2 === 0){
-        console.log('ERROR');
-        continue;
-    }
-    console.log(i)
-    }
-}
+// //1 
+// function isCount(a){
+// for (let i = 1; i <= 10; i++){
+//     if (i === a && a % 2 === 0){
+//         console.log('ERROR');
+//         continue;
+//     }
+//     console.log(i)
+//     }
+// }
 
-isCount(4)
-isCount(7)
+// isCount(4)
+// isCount(7)
 
-//2 while:
-let num = 20
-while(num > 0){
-    console.log(num)
-    num--
-}
+// //2 while:
+// let num = 20
+// while(num > 0){
+//     console.log(num)
+//     num--
+// }
 
-//2 while-do:
-do{
-    console.log(num)
-    num--
-} while(num > 0)
+// //2 while-do:
+// do{
+//     console.log(num)
+//     num--
+// } while(num > 0)
 
 
-//3
-let arrExample = ['Mango', 'Juice', 'Mouse', 'Hornet', 'Picture', 'Chinese']
+// //3
+// let arrExample = ['Mango', 'Juice', 'Mouse', 'Hornet', 'Picture', 'Chinese']
 
-for (element of arrExample){
-    console.log(element)
-}
+// for (element of arrExample){
+//     console.log(element)
+// }
 
 //4
 let objExample = {occupation: 'Spy',
@@ -44,4 +44,5 @@ objExample.occupation = 'Accountant'
 
 for (key in objExample){
     console.log(objExample[key])
+    console.log(key)
 }
---
+++ JavaScriptQA/JS/errorHandling.js
@@ -0,0 +1,39 @@
+//1
+let numArray = [2, 4, 6, 7, 8, 12, 64, 631];
+
+for (let i = 0; i <= numArray.length; i++) {
+  //Iterate over indices, go past 7
+  try {
+    if (numArray[i] !== undefined) {
+      console.log(numArray[i]); // Iterate over Array and print each element
+    } else if (numArray[i] === undefined) {
+      throw new Error("Out of bounds"); // Throw error if element is undefined
+    }
+  } catch (error) {
+    console.log("Error:", error.message); //Catch thrown error
+  }
+}
+
+//2
+let person = { firstName: "Peter", lastName: "Parker", city: "New York" };
+
+function objectPrint(a, b) {
+  if (a[b] !== undefined) {
+    console.log(a[b]); // Print value of given object[key]
+  }
+  try {
+    if (a[b] === undefined) {
+      throw new Error("No such key"); // Throw error if undefined
+    }
+  } catch (error) {
+    console.log("Error:", error.message); //Catch thrown error
+  }
+}
+try {
+  objectPrint(person, "firstName");
+  objectPrint(person, "blol");
+  objectPrint(pen, "blol");
+
+} catch (error) {
+  console.log("Error: No such object"); //Immediately catch ReferenceError of object
+}
